8a9e6d1b0025854187165b5ab9b4e64d65702bd6
MoreGutierrez <morelstuff2000@gmail.com>
2024-02-29 21:26:30 -0300

    Mi primer commit

---
+++ Clase1/ejercicio1.ts
@@ -0,0 +1,15 @@
+console.log("paso 1 cocinar una torta");
+console.log("paso 2 prender el horno");
+console.log("paso 3 preparar los ingredientes");
+console.log("paso 4 colocar ingredientes en un bowl");
+console.log("paso 5 batir");
+console.log("paso 6 enharinar el molde");
+console.log("paso 7 poner mezcla en el molde");
+console.log("paso 8 meter molde al horno");
+console.log("paso 9 poner cronometro");
+console.log("paso 10 meter un cuchillo en la parte de arriba de la torta y fijarse si está hecha");
+console.log("paso 11 si la torta está hecha se saca del horno");
+console.log("paso 12 apagar el horno");
+console.log("paso 13 desmoldar la torta y esperar que enfrie");
+console.log("paso 14 poner la torta en un plato y decorar");
+console.log("paso 15 comer la torta :)");
